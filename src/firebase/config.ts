dfcf23ccfcc7c92b99e7866d81605a033cd70b42
EOD_荒漠終焉 <eodofficial777@gmail.com>
2026-01-03 10:30:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5205305421-26a3a",
+  "appId": "1:422753622692:web:99d5ce828c7a1cf2d9e431",
+  "apiKey": "AIzaSyD062bSZwQ4EJvRb5QCjmV9qXcCQYPxtkI",
+  "authDomain": "studio-5205305421-26a3a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "422753622692"
+};
