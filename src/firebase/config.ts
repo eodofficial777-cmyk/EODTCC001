13fd489a8b572031e5206131cbd8bfdb813da218
EOD_荒漠終焉 <eodofficial777@gmail.com>
2026-01-05 10:45:35 +0000

    我目前已經建立好Realtime Database，這是他的參考網址https://studio-5205305421-26a3a-defaul

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:422753622692:web:99d5ce828c7a1cf2d9e431",
   "apiKey": "AIzaSyD062bSZwQ4EJvRb5QCjmV9qXcCQYPxtkI",
   "authDomain": "studio-5205305421-26a3a.firebaseapp.com",
+  "databaseURL": "https://studio-5205305421-26a3a-default-rtdb.asia-southeast1.firebasedatabase.app/",
   "measurementId": "",
   "messagingSenderId": "422753622692"
 };
